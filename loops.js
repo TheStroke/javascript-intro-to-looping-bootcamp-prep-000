4245bed0254ccfc110c4920f5985d51067686041
Aaron Gibson <aaron.gibson13@gmail.com>
2018-03-02 02:06:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,10 @@
 function forLoop (array) {
   for (let i = 0; i < 25; i++) {
-    if (i === 1) {
+    if (i === 0) {
+      array[0] = 'I am 0 strange loops.'
+    }
+    
+    else if (i === 1) {
       array.push('I am 1 strange loop.')
     }
     
